daa2ad2f066c6803c80b5ad2ba3324a4e5a19fca
อดิศร วงษ์รอด <vongrod001@gmail.com>
2025-07-08 16:29:59 +0700

    แก้ login และ config

--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,8 @@ document.getElementById("loginForm").addEventListener("submit", async function (
         if (dataRes.userInfo.status == 1) {
             const origin = window.location.origin       // https://mydomain.com
             const pathname = window.location.pathname  // /ncr-auto-admin/login        
-            const basePath = pathname.split('/')[0]    // "ncr-auto-admin"
+            const basePathMatch = pathname.match(/^\/([^/]+)/);
+            const basePath = (basePathMatch && basePathMatch[1] === 'ncr-auto-admin') ? 'ncr-auto-admin/' : '';
             const baseURL = `${origin}/${basePath}`    // https://mydomain.com/ncr-auto-admin
 
             // ใช้กับ fetch ได้เลย
@@ -27,8 +28,8 @@ document.getElementById("loginForm").addEventListener("submit", async function (
                 .then(res => res.json())
                 .then(data => {
                     console.log('Session set:', data)
-                    if(data.session.userDetail.Token){
-                         window.open(`${baseURL}link`,'_self')
+                    if (data.session.userDetail.Token) {
+                        window.open(`${baseURL}link`, '_self')
                     }
                 })
                 .catch(err => {
